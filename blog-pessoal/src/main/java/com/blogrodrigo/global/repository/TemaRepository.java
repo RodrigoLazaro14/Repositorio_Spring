47ff021e49e3be55e049109fdb4d3f7e83feae82
unknown <lazaro_db@hotmail.com>
2020-10-06 12:06:27 -0300

    Projeto blog pessoal

---
+++ blog-pessoal/src/main/java/com/blogrodrigo/global/repository/TemaRepository.java
@@ -0,0 +1,12 @@
+package com.blogrodrigo.global.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blogrodrigo.global.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
